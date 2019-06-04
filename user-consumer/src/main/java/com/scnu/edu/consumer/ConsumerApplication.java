17d85d890d4d964c89080e35874b51a5a1b0e527
Autom-liu <349576362@163.com>
2019-06-04 23:24:07 +0800

    换一种注解

--- user-consumer/src/main/java/com/scnu/edu/consumer/ConsumerApplication.java
+++ user-consumer/src/main/java/com/scnu/edu/consumer/ConsumerApplication.java
@@ -2,12 +2,12 @@ package com.scnu.edu.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableHystrix
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
