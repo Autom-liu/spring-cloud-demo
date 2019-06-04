4367f50b39ae4fce98a15a162ffb68308b5c9e89
Autom-liu <349576362@163.com>
2019-06-04 16:21:32 +0800

    使用ribbon负载均衡器

--- user-consumer/src/main/java/com/scnu/edu/consumer/web/ConsumerController.java
+++ user-consumer/src/main/java/com/scnu/edu/consumer/web/ConsumerController.java
@@ -1,10 +1,8 @@
 package com.scnu.edu.consumer.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +18,12 @@ public class ConsumerController {
 	@Autowired
 	private RestTemplate restTemplate;
 	@Autowired
-	private DiscoveryClient discoverClient;
+	private RibbonLoadBalancerClient ribbonClient;
 	
 	@GetMapping("/{id}")
 	public User queryById(@PathVariable("id") String id) {
-		// 根据服务id获取实例列表
-		List<ServiceInstance> instances = discoverClient.getInstances("user-service");
-		// 从实例列表中拿到实例
-		ServiceInstance instance = instances.get(0);
+		// 使用ribbon负载均衡器可以直接id实例
+		ServiceInstance instance = ribbonClient.choose("user-service");
 		String url = "http://"+ instance.getHost() +":"+ instance.getPort() +"/user/" + id;
 		User user = restTemplate.getForObject(url, User.class);
 		return user;
