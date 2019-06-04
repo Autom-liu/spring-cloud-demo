9a3e18f5040b925a60984b4f7c24430ad9dff704
Autom-liu <349576362@163.com>
2019-06-04 23:25:16 +0800

    再换一种注解

--- user-consumer/src/main/java/com/scnu/edu/consumer/ConsumerApplication.java
+++ user-consumer/src/main/java/com/scnu/edu/consumer/ConsumerApplication.java
@@ -1,15 +1,11 @@
 package com.scnu.edu.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableCircuitBreaker
-@EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 public class ConsumerApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConsumerApplication.class, args);
